11714faf2160956e7be5199f1bef7cefafb1e778
Awat Habibi <Awat.Habibi6@icloud.com>
2021-06-19 20:51:14 -0700

    Add README,md

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function()  {
+  $("form#question5").submit(function(event)  {
+    const question5 = $("select#question5").val();
+
+    if (question5 === "make websites" )  {
+      $("#result").show("JavaScript");
+    } else if (question5 === "2make apps") {
+      $("#result").show("Swift");
+    } else  {
+      $("result").show("Python")
+
+      event.preventDefault();
+    }
+  })
+})
